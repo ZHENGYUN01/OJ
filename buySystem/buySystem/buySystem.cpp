7ce974099a1eddf43349cffc87c94a81b6a73f8d
519309178@qq.com <519309178@qq.com>
2017-09-21 00:18:57 +0800

    buy system

--- buySystem/buySystem/buySystem.cpp
+++ buySystem/buySystem/buySystem.cpp
@@ -2,7 +2,13 @@
 //
 
 #include "stdafx.h"
+#include <vector>
+#include <map>
 
+using namespace std;
+
+#define SUCCESS 1
+#define FAIL 0
 
 typedef struct card
 {
@@ -17,17 +23,82 @@ typedef struct cargo
 	unsigned int num;
 }cargo;// 货物
 
-typedef struct order
+typedef struct Order
 {
 	map<unsigned int, unsigned int> goods;// <商品的种类，数量>
-}order;// 订单
+}Order;// 订单
 
 typedef struct shopTrolley
 {
-	vector<order> totalOrders;;
+	vector<Order> totalOrders;;
 	unsigned int totalBalance;
 }shopTrolley;// 购物车
 
+typedef struct BUYER
+{
+	shopTrolley myTrolley;
+	Order myOrder;
+	card myCard;
+}BUYER;// 一个购物者
+
+BUYER buyer;
+
+void initSystem()
+{
+	buyer.myCard.balance = 3000;
+	buyer.myCard.credit = 150;
+
+	buyer.myOrder.goods.clear();
+	buyer.myTrolley.totalOrders.clear();
+	buyer.myTrolley.totalBalance = 0;
+}
+
+void buyAction()
+{
+
+
+}
+
+void deleteAction()
+{
+
+
+}
+
+void queryCard()
+{
+	cout<<buyer.myCard.balance<<endl;
+	cout<<buyer.myCard.credit<<endl;
+}
+
+void queryTrolley()
+{
+	unsigned int numOfOrder = buyer.myTrolley.totalOrders.size();
+	vector<Order> totalOrders = buyer.myTrolley.totalOrders;
+
+	vector<Order>::iterator iter = totalOrders.begin();
+	for(; iter != totalOrders.end(); ++iter)
+	{
+		// 待完成
+	}
+}
+
+void queryAction(const char &type)
+{
+	if(type == '0')
+	{
+		queryCard();
+	}
+	else
+	{
+		queryTrolley();
+	}
+}
+
+void stopSystem()
+{
+    return SUCCESS;
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
